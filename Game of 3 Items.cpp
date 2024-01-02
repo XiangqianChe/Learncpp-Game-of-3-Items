9eadaaf0b4a314743c912846d1fd0ad7ae73d9b3
chexq <chexq35@gmail.com>
2024-01-02 15:33:36 -0500

    namespace Items and function countItems done

---
+++ Game of 3 Items.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <vector>
+#include <cassert>
+
+namespace Items
+{
+    enum Type
+    {
+        health_potion,
+        torch,
+        arrow,
+        max_items,
+    };
+}
+
+int countItems(const std::vector<int>& items)
+{
+    int count{};
+    for (auto item : items)
+        count += item;
+    return count;
+}
+
+int main()
+{
+    std::vector items{ 1, 5, 10 };
+    assert(items.size() == Items::max_items);
+    std::cout << "You have " << countItems(items) << " total items\n";
+
+    return 0;
+}
