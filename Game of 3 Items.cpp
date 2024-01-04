617a9a63e7ad014a04f1272d7bc2a6b7186ab287
chexq <chexq35@gmail.com>
2024-01-03 20:44:46 -0500

    change naming to inventory

--- Game of 3 Items.cpp
+++ Game of 3 Items.cpp
@@ -13,19 +13,20 @@ namespace Items
     };
 }
 
-int countItems(const std::vector<int>& items)
+int countItems(const std::vector<int>& inventory)
 {
     int count{};
-    for (auto item : items)
-        count += item;
+    for (auto e : inventory)
+        count += e;
     return count;
 }
 
+
 int main()
 {
-    std::vector items{ 1, 5, 10 };
-    assert(items.size() == Items::max_items);
-    std::cout << "You have " << countItems(items) << " total items\n";
+    std::vector inventory{ 1, 5, 10 };
+    assert(inventory.size() == Items::max_items);
+    std::cout << "You have " << countItems(inventory) << " total items\n";
 
     return 0;
 }
