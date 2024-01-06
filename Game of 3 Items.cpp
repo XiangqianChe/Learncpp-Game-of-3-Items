2015fb805faa5bec740ae113951147a5a75b2573
chexq <chexq35@gmail.com>
2024-01-05 22:31:01 -0500

    print items with numbers and names with proper pluralization

--- Game of 3 Items.cpp
+++ Game of 3 Items.cpp
@@ -13,6 +13,35 @@ namespace Items
     };
 }
 
+std::string_view getItemsSigular(Items::Type type)
+{
+    switch (type)
+    {
+    case Items::health_potion: return "health_potion";
+    case Items::torch: return "torch";
+    case Items::arrow: return "arrow";
+    default: return "???";
+    }
+}
+
+std::string_view getItemsPlural(Items::Type type)
+{
+    switch (type)
+    {
+    case Items::health_potion: return "health_potions";
+    case Items::torch: return "torches";
+    case Items::arrow: return "arrows";
+    default: return "???";
+    }
+}
+
+void printInventoryItem(const std::vector<int>& inventory, Items::Type type)
+{
+    bool plural{ inventory[type] != 1 };
+    std::cout << "You have " << inventory[type] << ' '
+        << (plural ? getItemsPlural(type) : getItemsSigular(type)) << '\n';
+}
+
 int countItems(const std::vector<int>& inventory)
 {
     int count{};
@@ -21,11 +50,12 @@ int countItems(const std::vector<int>& inventory)
     return count;
 }
 
-
 int main()
 {
     std::vector inventory{ 1, 5, 10 };
-    assert(inventory.size() == Items::max_items);
+    assert(std::size(inventory) == Items::max_items);
+    for (int index{}; index < Items::max_items; ++index)
+        printInventoryItem(inventory, static_cast<Items::Type>(index));
     std::cout << "You have " << countItems(inventory) << " total items\n";
 
     return 0;
